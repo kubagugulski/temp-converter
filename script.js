1b7279ddefa1704e2ae5501027b48cad6eeac879
Jakub Gugulski <jakub.gugulski1@gmail.com>
2022-05-02 14:14:21 +0200

    finished project

--- script.js
+++ script.js
@@ -12,22 +12,43 @@ const swap = () => {
     if (one.textContent == '°C') {
         one.textContent = '°F'
         two.textContent = '°C'
+        result.textContent = ''
     } else {
-    one.textContent = '°C'
-    two.textContent = '°F'
-}
-}
-
-const farToCel = () => {
-    farenheit = converter.value * 1.8 + 32
-    result.textContent = `${converter.value}°C equals ${farenheit.toFixed(1)}°F`
+        one.textContent = '°C'
+        two.textContent = '°F'
+        result.textContent = ''
+    }
 }
 
 const celToFar = () => {
+    farenheit = converter.value * 1.8 + 32
+    result.textContent = `${converter.value}°C equals ${farenheit.toFixed(1)}°F`
+    converter.value = ''
+}
+
+const farToCel = () => {
     celsius = (converter.value - 32) / 1.8
     result.textContent = `${converter.value}°F equals ${celsius.toFixed(1)}°C`
+    converter.value = ''
+}
+
+const conversion = () => {
+    if (converter.value !== '') {
+        if (one.textContent === '°C') {
+            celToFar()
+        } else {
+            farToCel()
+        }
+    } else {
+        result.textContent = 'No data!'
+    }
+}
+
+const reset = () => {
+    result.textContent = ''
+    converter.value = ''
 }
 
 swapBtn.addEventListener('click', swap)
-convertBtn.addEventListener('click', farToCel)
-// convertBtn.addEventListener('click', celToFar)
+convertBtn.addEventListener('click', conversion)
+resetBtn.addEventListener('click', reset)
